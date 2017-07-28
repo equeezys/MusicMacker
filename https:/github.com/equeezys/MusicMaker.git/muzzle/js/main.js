3f4c342b5b17b00e0b30a7b2d02f03631fa69ee7
Vlad <tvl95@bk.ru>
2017-07-28 11:54:11 +0300

    Deadline

--- https:/github.com/equeezys/MusicMaker.git/muzzle/js/main.js
+++ https:/github.com/equeezys/MusicMaker.git/muzzle/js/main.js
@@ -3,6 +3,9 @@ var q=1,
 _sor=document.getElementById("musicList"),
 _player = document.getElementById("player"),
 countUpdate=0,
+countUpdate2=0,
+countUpdate3=0,
+countUpdate4=0,
 iMus=0,
 loaded=0,pageindex=0
 
@@ -18,7 +21,9 @@ audioFiles = [[
                   "src/level3/set0.wav",
                   "src/level3/set1.wav",
                   "src/level3/set2.wav",
-                  "src/level3/set3.wav"
+                  "src/level3/set3.wav",
+                  "src/level4/set44.wav"
+                  
                   
               ],[3.75542,
                  2.27,
@@ -52,6 +57,12 @@ pageid = page ? page.id : "";
 	pageindex=parseInt(index);
 	if(pageid == "level2") pageindex=4+parseInt(index);
 	if(pageid == "level3") pageindex=8+parseInt(index);
+	if(pageid == "level4") {
+		if (parseInt(index)<5)
+		pageindex=(parseInt(index)+1)*4-1;
+		else 
+			pageindex=12;
+	}
     player.src = audioFiles[0][pageindex];
     count.backgroundColor = 'red';
     player.play();
@@ -93,6 +104,12 @@ window.onload = function() {
     	}
     });
     document.getElementById("musicList3").addEventListener('click', function(e) {
+    	if(q==0){
+    		q++;
+    		playlistItemClick(e.target);
+    	}
+    });
+    document.getElementById("musicList4").addEventListener('click', function(e) {
     	if(q==0){
     		q++;
     		playlistItemClick(e.target);
@@ -146,22 +163,23 @@ window.onload = function() {
 		    	}
 	    	}
 	 });
-       /*document.getElementById("reSortList").addEventListener('click', function(e) {
-    	   alert(q);
-    	   if(q==0){
-    		   $( "#sortablePlaylist" ).sortable("destroy");
-    	       $( "#sortablePlaylist" ).sortable({
-    	    	   change: function( event, ui ) {
-    	    		   countUpdate++;
-    	    		   $("#count").html(countUpdate);
-    	    	   }
-    	    	 });
-    		   $("#count").html(0);
-    		   countUpdate=0;
-    		   iMus=0;
-    	   }
-    	//alert(1);
-    });*/
+	 document.getElementById("playAll4").addEventListener('click', function(e) {
+		 var sortedIDs = $( "#sortablePlaylist4" ).sortable( "toArray" );
+	    	if(q==0){
+		    	if(sortedIDs[iMus]=="i4em"+iMus){
+		        	iMus++;
+		        	q=1;
+	    		document.getElementById("playerAll40").play();
+	    		document.getElementById("playerAll41").play();
+	    		document.getElementById("playerAll42").play();
+		    	}
+		    	else{
+		    		alert("WROOONG!!! "+iMus);
+		    		iMus=0;
+		    	}
+	    	}
+	 });
+	 //level1 start
 	 document.getElementById("playerAll0").addEventListener("ended", function(e) {
 	    	//alert("Event Listener "+iMus);
 	    	var sortedIDs = $( "#sortablePlaylist" ).sortable( "toArray" );
@@ -198,6 +216,8 @@ window.onload = function() {
 	    		q=0;
 	    	}
 	    });
+	 //level1 end
+	 //level2 start
 	 document.getElementById("playerAll20").addEventListener("ended", function(e) {
 	    	//alert("Event Listener "+iMus);
 	    	var sortedIDs = $( "#sortablePlaylist2" ).sortable( "toArray" );
@@ -211,26 +231,9 @@ window.onload = function() {
 	    		playerAll21.currentTime = 0;
 	    		playerAll22.pause();
 	    		playerAll22.currentTime = 0;
-	    		alert("WROOONG!!! "+iMus);
+	    		alert("WROOONG!!! lololo"+iMus);
 	    		//document.getElementById("playerAll1").stop();
 	    		//document.getElementById("playerAll2").stop();
-	    		iMus=0;
-	    		q=0;
-	    	}
-	    });
-	 document.getElementById("playerAll30").addEventListener("ended", function(e) {
-	    	var sortedIDs = $( "#sortablePlaylist3" ).sortable( "toArray" );
-	    	if(sortedIDs[iMus]=="i3em"+iMus){
-	        	iMus++;
-	    	}
-	    	else{
-	    		var playerAll21 = document.getElementById("playerAll21");
-	    		var playerAll22 = document.getElementById("playerAll22");
-	    		playerAll21.pause();
-	    		playerAll21.currentTime = 0;
-	    		playerAll22.pause();
-	    		playerAll22.currentTime = 0;
-	    		alert("WROOONG!!! "+iMus);
 	    		iMus=0;
 	    		q=0;
 	    	}
@@ -251,6 +254,8 @@ window.onload = function() {
 	    		q=0;
 	    	}
 	    });
+	 //level2 end
+	 //level3 start
 	 document.getElementById("playerAll31").addEventListener("ended", function(e) {
 	    	var sortedIDs = $( "#sortablePlaylist3" ).sortable( "toArray" );
 	    	if(sortedIDs[iMus]=="i3em"+iMus){
@@ -258,7 +263,61 @@ window.onload = function() {
 	    		
 	    	}
 	    	else{
-	    		var playerAll22 = document.getElementById("playerAll22");
+	    		var playerAll22 = document.getElementById("playerAll32");
+	    		playerAll22.pause();
+	    		playerAll22.currentTime = 0;
+	    		alert("WROOONG!!! "+iMus);
+	    		iMus=0;
+	    		q=0;
+	    	}
+	    });
+	 document.getElementById("playerAll30").addEventListener("ended", function(e) {
+	    	//alert("Event Listener "+iMus);
+	    	var sortedIDs = $( "#sortablePlaylist3" ).sortable( "toArray" );
+	    	if(sortedIDs[iMus]=="i3em"+iMus){
+	        	iMus++;
+	    	}
+	    	else{
+	    		var playerAll21 = document.getElementById("playerAll31");
+	    		var playerAll22 = document.getElementById("playerAll32");
+	    		playerAll21.pause();
+	    		playerAll21.currentTime = 0;
+	    		playerAll22.pause();
+	    		playerAll22.currentTime = 0;
+	    		alert("WROOONG!!! "+iMus);
+	    		//document.getElementById("playerAll1").stop();
+	    		//document.getElementById("playerAll2").stop();
+	    		iMus=0;
+	    		q=0;
+	    	}
+	    });
+	 //level3 end
+	 
+	 document.getElementById("playerAll40").addEventListener("ended", function(e) {
+	    	var sortedIDs = $( "#sortablePlaylist4" ).sortable( "toArray" );
+	    	if(sortedIDs[iMus]=="i4em"+iMus){
+	        	iMus++;
+	    	}
+	    	else{
+	    		var playerAll21 = document.getElementById("playerAll41");
+	    		var playerAll22 = document.getElementById("playerAll42");
+	    		playerAll21.pause();
+	    		playerAll21.currentTime = 0;
+	    		playerAll22.pause();
+	    		playerAll22.currentTime = 0;
+	    		alert("WROOONG!!! "+iMus);
+	    		iMus=0;
+	    		q=0;
+	    	}
+	    });
+	 document.getElementById("playerAll41").addEventListener("ended", function(e) {
+	    	var sortedIDs = $( "#sortablePlaylist4" ).sortable( "toArray" );
+	    	if(sortedIDs[iMus]=="i4em"+iMus){
+	    		iMus++;
+	    		
+	    	}
+	    	else{
+	    		var playerAll22 = document.getElementById("playerAll42");
 	    		playerAll22.pause();
 	    		playerAll22.currentTime = 0;
 	    		alert("WROOONG!!! "+iMus);
@@ -275,31 +334,29 @@ window.onload = function() {
 	    });
 	 document.getElementById("playerAll22").addEventListener("ended", function(e) {
 	    	//alert("Event Listener "+iMus);
-		 		alert("Congrats you win!Your score : "+countUpdate+" .");
+		 		alert("Congrats you win!Your score : "+countUpdate2+" .");
 				iMus=0;
-				countUpdate=0;
+				countUpdate2=0;
 				q=0;
 	    });
 	 document.getElementById("playerAll32").addEventListener("ended", function(e) {
 	    	//alert("Event Listener "+iMus);
-		 		alert("Congrats you win!Your score : "+countUpdate+" .");
+		 		alert("Congrats you win!Your score : "+countUpdate3+" .");
 				iMus=0;
-				countUpdate=0;
+				countUpdate3=0;
+				q=0;
+	    });
+	 document.getElementById("playerAll42").addEventListener("ended", function(e) {
+	    	//alert("Event Listener "+iMus);
+		 		alert("Congrats you win!Your score : "+countUpdate4+" .");
+				iMus=0;
+				countUpdate4=0;
 				q=0;
 	    });
 	 
     document.getElementById("player").addEventListener("ended", function(e) {
     	//alert("Event Listener "+iMus);
-        if (iMus>0) {
-        	playNext();
-            if(iMus < 3) playlistItemClick(e.target);
-            	else {
-            		alert("Congrats you win!Your score : "+countUpdate+" .");
-            		iMus=0;
-            		countUpdate=0;
-            }
-          }
-        else     	playNext();
+    	playNext();
     	//alert(1);
     });
     
@@ -315,8 +372,8 @@ window.onload = function() {
        $( "#sortablePlaylist2" ).sortable({
     	   change: function( event, ui ) {
     		   if (q==0){
-    			   countUpdate++;
-    			   $("#count2").html(countUpdate);
+    			   countUpdate2++;
+    			   $("#count2").html(countUpdate2);
     		   }
     		   else $(this).sortable( "cancel" );
     	   }
@@ -324,8 +381,19 @@ window.onload = function() {
        $( "#sortablePlaylist3" ).sortable({
     	   change: function( event, ui ) {
     		   if (q==0){
-    			   	countUpdate++;
-    		   		$("#count3").html(countUpdate);
+    			   	countUpdate3++;
+    		   		$("#count3").html(countUpdate3);
+    		   }
+    		   else 
+    			   $(this).sortable( "cancel" );
+    		   
+    	}
+    	 });
+       $( "#sortablePlaylist4" ).sortable({
+    	   change: function( event, ui ) {
+    		   if (q==0){
+    			   	countUpdate4++;
+    		   		$("#count3").html(countUpdate4);
     		   }
     		   else 
     			   $(this).sortable( "cancel" );
